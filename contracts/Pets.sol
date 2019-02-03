5257313964bff2bdf0c8a8e9bbe9cfa048ea16c0
ajlopez <webmaster@ajlopez.com>
2019-02-03 18:43:37 -0300

    Initial pet

--- contracts/Pets.sol
+++ contracts/Pets.sol
@@ -3,7 +3,22 @@ pragma solidity ^0.4.24;
 contract Pets {
     address public owner;
     
-    constructor() public {
+    struct Pet {
+        uint genes;
+    }
+    
+    Pet[] pets;
+  
+    constructor(uint _genes) public {
         owner = msg.sender;
+        pets.push(Pet(_genes));
+    }
+    
+    function getPet(uint id) public view returns (uint genes) {
+        return pets[id].genes;
+    }
+    
+    function noPets() public view returns (uint size) {
+        return pets.length;
     }
 }
--- test/Pets_test.js
+++ test/Pets_test.js
@@ -5,12 +5,20 @@ contract('Pets', function (accounts) {
     const creator = accounts[0];
     
     beforeEach(async function () {
-        this.pets = await Pets.new();
+        this.pets = await Pets.new(1);
     });
     
     it('creator is owner', async function () {
         const owner = await this.pets.owner();
         
         assert.equal(owner, creator);
+    });
+    
+    it('initial pet', async function () {
+        const nopets = await this.pets.noPets();
+        assert.equal(nopets, 1);
+        
+        const genes = await this.pets.getPet(0);
+        assert.equal(genes, 1);
     });
 });
